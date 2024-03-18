cffbfdcffe308ce7477146415dc2fdcda2960361
shin <sinej1020@gmail.com>
2024-03-18 23:50:30 +0900

    group - World, Local 좌표계

--- r3f_basic/src/ThreeElements.tsx
+++ r3f_basic/src/ThreeElements.tsx
@@ -5,31 +5,64 @@ import { useFrame } from '@react-three/fiber'
 
 const ThreeElements = () => {
  const boxRef = useRef<THREE.Mesh>(null);
+ const groupRef = useRef<THREE.Group>(null);
     useFrame((state, delta) => {
-        boxRef.current.rotation.x += delta;
-        boxRef.current.position.x += 0.01;
-        boxRef.current.scale.x += 0.01;
+        // boxRef.current.rotation.x += delta;
+        // boxRef.current.position.x += 0.01;
+        // boxRef.current.scale.x += 0.01;
+        // scence.rotation.x = THREE.MathUtils.degToRad(45)
+        // scence.rotation.x += 0.01;
+        groupRef.current.rotation.x += delta;
     })
 
 
 
  return (
-  <>
-      <directionalLight position={[5,5,5]}/>
-      <mesh ref={boxRef}
-            position={[0,0,0]}
-            scale={[1,1,1]}
-            rotation={[
-                THREE.MathUtils.degToRad(0),
-                THREE.MathUtils.degToRad(0),
-                THREE.MathUtils.degToRad(0),
-            ]}
-      >
-          <boxGeometry />
-          {/*<sphereGeometry/> 원형 */}
-          <meshStandardMaterial color="red"/>
-      </mesh>
-  </>
+     <>
+         <directionalLight position={[5, 5, 5]}/>
+         <group position={[0,0,0]} ref={groupRef}>
+             <mesh ref={boxRef}
+                   position={[0, 0, 0]}
+                   scale={[1, 1, 1]}
+                   rotation={[
+                       THREE.MathUtils.degToRad(0),
+                       THREE.MathUtils.degToRad(0),
+                       THREE.MathUtils.degToRad(0),
+                   ]}
+             >
+                 <boxGeometry/>
+                 {/*<sphereGeometry/> 원형 */}
+                 <meshStandardMaterial color="red"/>
+             </mesh>
+             <mesh ref={boxRef}
+                   position={[0, 2, 0]}
+                   scale={[1, 1, 1]}
+                   rotation={[
+                       THREE.MathUtils.degToRad(0),
+                       THREE.MathUtils.degToRad(0),
+                       THREE.MathUtils.degToRad(0),
+                   ]}
+             >
+                 <boxGeometry/>
+                 {/*<sphereGeometry/> 원형 */}
+                 <meshStandardMaterial color="green"/>
+             </mesh>
+             <mesh ref={boxRef}
+                   position={[2, 0, 0]}
+                   scale={[1, 1, 1]}
+                   rotation={[
+                       THREE.MathUtils.degToRad(0),
+                       THREE.MathUtils.degToRad(0),
+                       THREE.MathUtils.degToRad(0),
+                   ]}
+             >
+                 <axesHelper args={[3]}/>
+                 <boxGeometry/>
+                 {/*<sphereGeometry/> 원형 */}
+                 <meshStandardMaterial color="blue"/>
+             </mesh>
+         </group>
+     </>
  );
 }
 
