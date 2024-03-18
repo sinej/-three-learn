90378fa0398b45670bc36c615eb1efbde158a242
shin <sinej1020@gmail.com>
2024-03-18 22:29:00 +0900

    Controls, Helper 실습

--- r3f_basic/src/App.tsx
+++ r3f_basic/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import {Canvas} from "@react-three/fiber";
 import ThreeElements from "./ThreeElements.tsx";
+import {OrbitControls} from "@react-three/drei";
 
 function App() {
 
@@ -15,6 +16,17 @@ function App() {
                 position: [3,3,0]
             }}
         >
+            {/* Math.PI = 180 */}
+            {/* Math.PI * 2 = 360 */}
+            {/* Math.PI / 4 = 45 */}
+            <OrbitControls
+                // minAzimuthAngle={-Math.PI / 4}
+                // maxAzimuthAngle={Math.PI / 4}
+                // minPolarAngle={Math.PI / 6}
+                // maxPolarAngle={Math.PI - Math.PI / 6}
+            />
+            <axesHelper args={[5]}/>
+            <gridHelper args={[20,20,0xff0000,'teal']}/>
             <ThreeElements/>
         </Canvas>
     </>
