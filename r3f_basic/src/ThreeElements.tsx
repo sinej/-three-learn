5d8b305b76e3bc9cb2498c7fc7c96421cf1f7012
shin <sinej1020@gmail.com>
2024-03-19 22:17:48 +0900

    wireFrame, geometry 공유, boxGeometry 옵션

--- r3f_basic/src/ThreeElements.tsx
+++ r3f_basic/src/ThreeElements.tsx
@@ -1,18 +1,33 @@
-import { useRef } from "react";
+import {useEffect, useRef} from "react";
 import * as THREE from 'three';
 import { useFrame } from '@react-three/fiber'
-import {Box, Cone, Sphere} from "@react-three/drei";
+import { useControls } from "leva";
+
 const ThreeElements = () => {
     const boxRef = useRef<THREE.Mesh>(null);
+    const boxCopyRef = useRef<THREE.Mesh>(null);
 
-    useFrame((state, delta) => {
+    const boxControl = useControls({
+        width: {value: 1, min: 0.1, max: 10, step: 0.1},
+        height: {value: 1, min: 0.1, max: 10, step: 0.1},
+        depth: {value: 1, min: 0.1, max: 10, step: 0.1},
+        widthSeg: {value: 1, min: 0.1, max: 10, step: 0.1},
+        heightSeg: {value: 1, min: 0.1, max: 10, step: 0.1},
+        depthSeg: {value: 1, min: 0.1, max: 10, step: 0.1},
+    })
+
+    // useFrame((state, delta) => {
         // boxRef.current.rotation.x += delta;
         // boxRef.current.position.x += 0.01;
         // boxRef.current.scale.x += 0.01;
         // scence.rotation.x = THREE.MathUtils.degToRad(45)
         // scence.rotation.x += 0.01;
         // groupRef.current.rotation.x += delta;
-    })
+    // })
+
+    useEffect(() => {
+        boxCopyRef.current.geometry = boxRef.current.geometry;
+    }, [boxControl])
 
     // const geometry = new THREE.BoxGeometry(1,1,1);
     // const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
@@ -24,24 +39,32 @@ const ThreeElements = () => {
     return (
         <>
             <directionalLight position={[5,5,5]}/>
-            <Sphere position={[2, 0, 0]}>
-                <meshStandardMaterial color="green"/>
-            </Sphere>
-            <Cone>
-                <meshStandardMaterial color="green"/>
-            </Cone>
-            <Box position={[-2, 0, 0]}>
-                <meshStandardMaterial color="green"/>
-            </Box>
-            {/*<mesh geometry={new THREE.BoxGeometry(1,1,1)}>*/}
-            {/*    <meshStandardMaterial color="blue"/>*/}
-            {/*</mesh>*/}
-            {/*<mesh ref={boxRef}*/}
-            {/*   position={[2, 0, 0]}*/}
-            {/*>*/}
-            {/*    <boxGeometry/>*/}
-            {/*    <meshStandardMaterial color="red"/>*/}
-            {/*</mesh>*/}
+            {/*<Sphere position={[2, 0, 0]}>*/}
+            {/*    <meshStandardMaterial color="green"/>*/}
+            {/*</Sphere>*/}
+            {/*<Cone>*/}
+            {/*    <meshStandardMaterial color="green"/>*/}
+            {/*</Cone>*/}
+            {/*<Box position={[-2, 0, 0]}>*/}
+            {/*    <meshStandardMaterial color="green"/>*/}
+            {/*</Box>*/}
+            <mesh
+                ref={boxRef}
+               position={[0, 0, 0]}
+            >
+                <boxGeometry args={[
+                    boxControl.width,
+                    boxControl.height,
+                    boxControl.depth,
+                    boxControl.widthSeg,
+                    boxControl.heightSeg,
+                    boxControl.depthSeg]}
+                />
+                <meshStandardMaterial wireframe />
+            </mesh>
+            <mesh ref={boxCopyRef}>
+                <meshStandardMaterial color="red"/>
+            </mesh>
         </>
     );
 }
